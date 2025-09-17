5032845b78404e661923b4500e6ad14180d95c42
DevinBayly <baylyd@email.arizona.edu>
2025-09-17 12:42:23 -0700

    commiting to finish merge, but only for jpgs and script files, database vcs will be handled separately

--- openalex_code/hear_me_ROR_script.py
+++ openalex_code/hear_me_ROR_script.py
@@ -146,7 +146,7 @@ def add_institution_to_db(con: db.DuckDBPyConnection, institution_ror: str = Non
 
     Args:
         con (db.DuckDBPyConnection): connection to the database
-        institution_roi (str): ROR identification of the institution
+        institution_ror (str): ROR identification of the institution
 
     Returns:
         int: id of the institution
@@ -233,7 +233,6 @@ def populate_database(database_file: str, ror: str, years: range, content_root: 
                         con.execute(f"""INSERT INTO author VALUES ({a['author']['id'].split('A')[-1]}, '{a['author']['display_name'].replace("'", "")}');""")
                     except db.ConstraintException:
                         pass
-                        continue
 
                     try:
                         con.execute(f"INSERT INTO contribution VALUES ({a['author']['id'].split('A')[-1]}, {pub['id'].split('W')[-1]});")
